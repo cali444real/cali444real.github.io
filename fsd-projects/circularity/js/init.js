e52f916f1fe80f2307b95206f4aebd9899de20d2
cali444real <cali.ravenpilate@gmail.com>
2026-05-13 03:39:55 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -28,12 +28,16 @@ var init = function (window) {
     }
 
     // TODO 3 : Call the drawCircle() function
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
+    // drawCircle();
+    // drawCircle();
+    // drawCircle();
+    // drawCircle();
+    // drawCircle();
+
     // TODO 7 : Use a loop to create multiple circles
+    for (var circle = 0; circles.length < 50; circle++) {
+      drawCircle();
+    }
 
     ///////////////////
     // PROGRAM LOGIC //
@@ -46,18 +50,22 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
-      physikz.updatePosition(circles[0]);
-      physikz.updatePosition(circles[1]);
-      physikz.updatePosition(circles[2]);
-      physikz.updatePosition(circles[3]);
-      physikz.updatePosition(circles[4]);
+      //      physikz.updatePosition(circles[0]);
+      //      physikz.updatePosition(circles[1]);
+      //      physikz.updatePosition(circles[2]);
+      //      physikz.updatePosition(circles[3]);
+      //      physikz.updatePosition(circles[4]);
       // TODO 5 : Call game.checkCirclePosition() on your circles
-      game.checkCirclePosition(circles[0]);
-      game.checkCirclePosition(circles[1]);
-      game.checkCirclePosition(circles[2]);
-      game.checkCirclePosition(circles[3]);
-      game.checkCirclePosition(circles[4]);
+      //      game.checkCirclePosition(circles[0]);
+      //      game.checkCirclePosition(circles[1]);
+      //      game.checkCirclePosition(circles[2]);
+      //      game.checkCirclePosition(circles[3]);
+      //      game.checkCirclePosition(circles[4]);
       // TODO 8 / TODO 9 : Iterate over the array
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
     }
 
     /* 
@@ -74,7 +82,12 @@ var init = function (window) {
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
       if (circle.x < 0) {
         circle.x = canvas.width;
+      } else if (circle.y < 0) {
+        circle.y = canvas.height;
+      } else if (circle.y > canvas.height) {
+        circle.y = 0;
       }
+
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
     };
 
